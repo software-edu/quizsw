b8f4b12cd7a3d8b6b174b5282df947f0eeb4d51d
Luh <Luh@DESKTOP-4S7PQO9>
2016-03-29 23:31:42 -0300

    view

--- src/br/org/uesb/quizsw/view/Janela_Escolha_Usuario.java
+++ src/br/org/uesb/quizsw/view/Janela_Escolha_Usuario.java
@@ -1,17 +1,13 @@
 package br.org.uesb.quizsw.view;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
 
+import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JLabel;
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
